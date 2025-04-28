eb9c31dd89a1900a68c785d0567ead8ce9dad2ec
wjdwo <wjdwo3552@gmail.com>
2025-04-29 01:34:41 +0900

    1

---
+++ src/main/java/com/gravifox/tvb/domain/member/dto/mypage/MyInfoResponse.java
@@ -0,0 +1,10 @@
+package com.gravifox.tvb.domain.member.dto.mypage;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MyInfoResponse {
+    private String userId;
+}
