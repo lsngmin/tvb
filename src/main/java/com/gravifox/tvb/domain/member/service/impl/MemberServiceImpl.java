17241f8754b2b53afb9ef22b1798434137a7dcef
wjdwo <wjdwo3552@gmail.com>
2025-05-05 02:08:25 +0900

    코드리뷰 수정

--- src/main/java/com/gravifox/tvb/domain/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/gravifox/tvb/domain/member/service/impl/MemberServiceImpl.java
@@ -1,9 +1,9 @@
 package com.gravifox.tvb.domain.member.service.impl;
 
-import com.gravifox.tvb.domain.member.domain.user.User;
 import com.gravifox.tvb.domain.member.dto.mypage.MyInfoResponse;
 import com.gravifox.tvb.domain.member.repository.UserRepository;
 import com.gravifox.tvb.domain.member.service.MemberService;
+import com.gravifox.tvb.domain.member.exception.user.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,11 +15,13 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public MyInfoResponse getMyInfo(Long userNo) {
-        User user = userRepository.findById(userNo)
-                .orElseThrow(() -> new RuntimeException("회원 정보를 찾을 수 없습니다."));
-
-        return MyInfoResponse.builder()
-                .userId(user.getUserId())
-                .build();
+        return userRepository.findById(userNo)
+                .map(user -> MyInfoResponse.builder()
+                        .userId(user.getUserId())
+                        .loginType(user.getLoginType().name())
+                        .nickname(user.getProfile().getNickname())
+                        .createdAt(user.getProfile().getCreatedAt())
+                        .build())
+                .orElseThrow(() -> new UserNotFoundException(userNo));
     }
 }
