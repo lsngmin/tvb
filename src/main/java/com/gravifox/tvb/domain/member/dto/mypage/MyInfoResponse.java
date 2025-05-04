7ac34318e8bed690e1dafca1a042c393f2485d03
wjdwo <wjdwo3552@gmail.com>
2025-05-04 16:56:55 +0900

    코드리뷰 수정

--- src/main/java/com/gravifox/tvb/domain/member/dto/mypage/MyInfoResponse.java
+++ src/main/java/com/gravifox/tvb/domain/member/dto/mypage/MyInfoResponse.java
@@ -1,14 +1,34 @@
 package com.gravifox.tvb.domain.member.dto.mypage;
 
+import com.gravifox.tvb.domain.member.dto.AuthDTO;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Schema(description = "내 정보 조회 응답 DTO")
 @Getter
 @Builder
-public class MyInfoResponse {
+public class MyInfoResponse implements AuthDTO {
 
-    @Schema(description = "사용자의 이메일 또는 이메일", example = "user@example.com", required = true)
-    private String userId;
+    @Schema(description = "사용자 고유 번호", example = "1")
+    private final Long userNo;
+
+    @Schema(description = "사용자의 이메일", example = "user@example.com")
+    private final String userId;
+
+    @Schema(description = "사용자 닉네임", example = "홍길동")
+    private final String nickname;
+
+    @Schema(description = "사용자 가입일", example = "2024-01-01T12:34:56")
+    private final LocalDateTime createdAt;
+
+    @Schema(description = "로그인 유형 (예: LOCAL, KAKAO)", example = "LOCAL")
+    private final String loginType;
+
+    @Override
+    public String extractUserID() {
+        return this.userId;
+    }
 }
