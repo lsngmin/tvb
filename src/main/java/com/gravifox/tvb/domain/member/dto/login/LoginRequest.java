f12d0f9037e69066be5f43285d2ed78cb628f0ed
wjdwo <wjdwo3552@gmail.com>
2025-05-04 16:58:02 +0900

    코드리뷰 수정

--- src/main/java/com/gravifox/tvb/domain/member/dto/login/LoginRequest.java
+++ src/main/java/com/gravifox/tvb/domain/member/dto/login/LoginRequest.java
@@ -17,10 +17,25 @@ import java.util.Map;
 @NoArgsConstructor
 public class LoginRequest implements AuthDTO {
 
-    @Schema(description = "로그인에 사용할 사용자 정보", required = true)
+    @Schema(description = "로그인에 사용할 사용자 정보",
+            required = true,
+            example = """
+        {
+            "userId": "user@example.com",
+            "userNo": 1
+        }
+        """
+    )
     private User user;
 
-    @Schema(description = "사용자 비밀번호 정보", required = true)
+    @Schema(description = "사용자 비밀번호 정보",
+            required = true,
+            example = """
+        {
+            "password": "encrypted_password"
+        }
+        """
+    )
     private Password password;
 
     public Map<String, String> getDataMap() {
