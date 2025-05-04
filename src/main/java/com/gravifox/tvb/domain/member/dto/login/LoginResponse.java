1589ef6aa353543b1ee675e0d4889b841c990c21
wjdwo <wjdwo3552@gmail.com>
2025-05-04 16:57:11 +0900

    코드리뷰 수정

--- src/main/java/com/gravifox/tvb/domain/member/dto/login/LoginResponse.java
+++ src/main/java/com/gravifox/tvb/domain/member/dto/login/LoginResponse.java
@@ -8,10 +8,18 @@ import lombok.Data;
 @Data
 public class LoginResponse implements AuthDTO {
 
-    @Schema(description = "사용자 이메일 또는 아이디", example = "user@example.com", required = true)
+    @Schema(
+            description = "사용자의 이메일",
+            example = "user@example.com",
+            required = true
+    )
     private String userId;
 
-    @Schema(description = "로그인 성공 시 발급되는 액세스 토큰", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...", required = true)
+    @Schema(
+            description = "로그인 성공 시 발급되는 JWT 액세스 토큰",
+            example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+            required = true
+    )
     private String accessToken;
 
     @Override
