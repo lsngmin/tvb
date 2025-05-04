80234da7d6ef334a52389011bf5ae09332dbf6bd
wjdwo <wjdwo3552@gmail.com>
2025-05-05 02:07:56 +0900

    코드리뷰 수정

--- src/main/java/com/gravifox/tvb/domain/member/exception/common/ErrorMessageMap.java
+++ src/main/java/com/gravifox/tvb/domain/member/exception/common/ErrorMessageMap.java
@@ -1,11 +1,17 @@
 package com.gravifox.tvb.domain.member.exception.common;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@Schema(description = "API 에러 응답 형식")
 @Getter
 @AllArgsConstructor
 public class ErrorMessageMap {
+
+    @Schema(description = "에러 코드", example = "INVALID_CREDENTIALS")
     private String code;
+
+    @Schema(description = "에러 메세지", example = "Oops! Something doesn’t match.")
     private String message;
 }
