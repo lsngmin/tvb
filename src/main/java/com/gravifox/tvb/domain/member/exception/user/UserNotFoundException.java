b04409be644ff2a52f5ae105d36c6fd1340a0a74
wjdwo <wjdwo3552@gmail.com>
2025-05-07 17:52:06 +0900

    UserException 상속으로 수정

--- src/main/java/com/gravifox/tvb/domain/member/exception/user/UserNotFoundException.java
+++ src/main/java/com/gravifox/tvb/domain/member/exception/user/UserNotFoundException.java
@@ -1,9 +1,9 @@
 package com.gravifox.tvb.domain.member.exception.user;
 
-import com.gravifox.tvb.domain.member.exception.auth.AuthException;
 import com.gravifox.tvb.domain.member.exception.common.ErrorCode;
 
-public class UserNotFoundException extends AuthException {
+public class UserNotFoundException extends UserException {
+
     public UserNotFoundException(Long userNo) {
         super(ErrorCode.USER_NOT_FOUND, String.valueOf(userNo));
     }
