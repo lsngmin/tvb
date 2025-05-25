8e94e23dc0c871f76d28a7d4d7f184e969cf55a0
wjdwo <wjdwo3552@gmail.com>
2025-05-25 21:19:30 +0900

    feat(member): 사용자 비밀번호 변경 기능 구현 #44

--- src/main/java/com/gravifox/tvb/domain/member/repository/PasswordRepository.java
+++ src/main/java/com/gravifox/tvb/domain/member/repository/PasswordRepository.java
@@ -12,5 +12,5 @@ public interface PasswordRepository extends JpaRepository<Password, Long> {
 
     @Query("SELECT p.password FROM Password p WHERE p.user = :user")
     Optional<String> findPasswordByUser(@Param("user") User user);
-
+    Optional<Password> findByUser(User user);
 }
