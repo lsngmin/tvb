676564c5cfe6f6f4f2274946e18f673ee7629486
sngmin <lsgmin@naver.com>
2025-07-08 13:24:14 +0900

    hotfix: 리다이렉트 주소 변경

--- src/main/java/com/gravifox/tvb/domain/admin/repository/TermRepository.java
+++ src/main/java/com/gravifox/tvb/domain/admin/repository/TermRepository.java
@@ -1,5 +1,9 @@
 package com.gravifox.tvb.domain.admin.repository;
 
 import com.gravifox.tvb.domain.admin.domain.Term;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
+public interface TermRepository extends JpaRepository<Term, Long> {
 }
+
